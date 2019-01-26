088fd4d8349936b27621a6aac135626e1c4cd294
Cyril MORISSE <cmorisse@boxes3.net>
2019-01-26 09:23:27 +0000

    IMP: Adapt for Odoo 11 and 12

--- src/inouk/recipe/odoo_cmd.py
+++ src/inouk/recipe/odoo_cmd.py
@@ -2,8 +2,12 @@
 """
 odoo.py command for Odoo servers built using anybox.recipe.odoo
 """
-__version__ = '0.1.0'
-from odoo import main
+__version__ = '0.2.0'
+
+try:
+    from odoo import main
+except:
+    from odoo.cli.command import main
 
 
 def buildout_entry_point():
